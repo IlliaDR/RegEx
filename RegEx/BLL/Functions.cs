cb151bd1e2fffe645afe22620a20d2cffea50ddf
Illia Drachuk <idrac@softserveinc.com>
2023-07-08 13:12:06 +0300

    UC#10 Implement regex

---
+++ RegEx/BLL/Functions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BLL
+{
+	public class Functions
+	{
+		public static bool ValidateString(string str, int maxLength)
+		{
+			//Check length
+			if (str.Length > maxLength)
+				return false;
+
+			//Check lowercase, uppercase, digit, special character
+			string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!""#$%&'()*+,-./:;<=>?@[\]^_`{|}~])";
+
+			//Check whitespace
+			pattern += @"[^\s]{" + str.Length + "}$";
+
+			return Regex.IsMatch(str, pattern);
+		}
+	}
+}
